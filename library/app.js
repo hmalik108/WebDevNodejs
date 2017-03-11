07c2f579150d6bb5ac54ed90a9834e764a9fa177
Harish Malik <hmalik2@asu.edu>
2017-03-11 02:36:34 -0700

    Adding start info in package.json

--- library/app.js
+++ library/app.js
@@ -3,6 +3,14 @@ var express = require('express')
 var app = express();
 var port = 8082;
 
-app.addEventListener(8082, function(err){
-    console.log('magic happend on port'+ port);
+app.get('/' , function(req,res){
+    res.send('Hello World');
+});
+
+app.get('/books', function(req, res){
+    res.send('Hello books');
+});
+
+app.listen(8082, function(err){
+    console.log('magic happens on port'+ port);
 })
