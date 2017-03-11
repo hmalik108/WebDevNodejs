0905081c9160fb0479b28afc773a8cfb2f50262d
Harish Malik <hmalik2@asu.edu>
2017-03-11 05:24:52 -0700

    feat: Gulp | Coding Stds | Coding Styles Added Gulp for Task Manager Added JSHint for Coding Standards Added JSCS for Coding Style

--- library/app.js
+++ library/app.js
@@ -1,7 +1,7 @@
-var express = require('express')
+var express = require('express');
 
 var app = express();
-var port = 8082;
+var port = process.env.PORT || 8081;
 
 
 app.use(express.static('public'));
@@ -9,14 +9,14 @@ app.use(express.static('src/views'));
 
 
 
-app.get('/' , function(req,res){
+app.get('/', function (req, res) {
     res.send('Hello World');
 });
 
-app.get('/books', function(req, res){
+app.get('/books', function (req, res) {
     res.send('Hello books');
 });
 
-app.listen(8082, function(err){
-    console.log('magic happens on port'+ port);
-})
+app.listen(port, function (err) {
+    console.log('magic happens on port' + port);
+});
