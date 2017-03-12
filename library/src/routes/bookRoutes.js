5e15d20075435f6ea8d81d4e371c85d494e61660
Harish Malik <hmalik2@asu.edu>
2017-03-12 06:25:56 -0700

    feat: Mongodb findOne

--- library/src/routes/bookRoutes.js
+++ library/src/routes/bookRoutes.js
@@ -2,6 +2,7 @@ var express = require('express');
 
 var bookRouter = express.Router();
 var mongodb = require('mongodb').MongoClient;   
+var objectId = require('mongodb').ObjectId;   
 
 
 var router = function (nav) {
@@ -14,7 +15,7 @@ var router = function (nav) {
             mongodb.connect(url, function (err, db) {
                 var collection = db.collection('books');
                 collection.find({}).toArray(function (err, results) {
-                    console.log(results);
+                    //console.log(results);
                     res.render('bookListView', {
                         title: 'bookListView',
                         nav: nav,
@@ -29,11 +30,19 @@ var router = function (nav) {
 
     bookRouter.route('/:id')
         .get(function (req, res) {
-            var id = req.params.id;
-            res.render('bookView', {
-                title: 'bookView',
-                nav: nav,
-                book: books[id]
+            var id = new objectId(req.params.id);
+            var url = 'mongodb://localhost:27017/libraryApp';
+            mongodb.connect(url, function (err, db) {
+                var collection = db.collection('books');
+                collection.findOne({_id: id}, function (err, results) {
+                    //console.log(results);
+                    res.render('bookView', {
+                        title: 'bookView',
+                        nav: nav,
+                        book: results
+                    });
+                    db.close();
+                });
             });
         });
 
