c735f3ce793a26f71c9e81057d53d97d184a7629
Harish Malik <hmalik2@asu.edu>
2017-03-11 18:10:03 -0700

    feat: Rendering Rendering with EJS template engine

--- library/app.js
+++ library/app.js
@@ -13,17 +13,58 @@ app.set('views', './src/views');
 
 app.set('view engine', 'ejs');
 
+var books = [
+    
+    {
+        title: 'Book Title 1',
+        genre: 'Hstorical',
+        author: 'Adam Dan',
+        read: false
+    },
+    {
+        title: 'Book Title 2',
+        genre: 'Hstorical',
+        author: 'Adam Dan',
+        read: false
+    },
+    {
+        title: 'Book Title 3',
+        genre: 'Hstorical',
+        author: 'Adam Dan',
+        read: false
+    },
+    {
+        title: 'Book Title 4',
+        genre: 'Hstorical',
+        author: 'Adam Dan',
+        read: false
+    }
+    
+    
+]
+
+
 bookRouter.route('/')
-    .get(function(req,res){
-        res.send("Hello Books");
+    .get(function (req, res) {
+        res.render('books', {
+            title: ' Hello from ejs',
+            nav: [{
+                Link: '/Books',
+                Text: 'Books'
+        }, {
+                Link: '/Author',
+                Text: 'Authors'
+        }],
+            books: books
+        });
     });
 
 bookRouter.route('/single')
-    .get(function(req,res){
+    .get(function (req, res) {
         res.send("Hello Single Books");
     });
 
-app.use('/',bookRouter);
+app.use('/', bookRouter);
 
 app.get('/', function (req, res) {
     res.render('index', {
