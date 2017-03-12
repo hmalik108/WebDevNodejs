85790c4580f20bc7df35d243beb738b95073b6f1
Harish Malik <hmalik2@asu.edu>
2017-03-12 05:52:07 -0700

    feat: MongoDB Added MongoDB to the app. Added a Insert command to insert books.

--- library/src/routes/adminRoutes.js
+++ library/src/routes/adminRoutes.js
@@ -1,16 +1,57 @@
 var express = require('express');
 
 var adminRouter = express.Router();
+var mongodb = require('mongodb').MongoClient;
 
-var router = function(nav){
+var books = [
+
+    {
+        title: 'Scion of Ikshvaku',
+        genre: 'Mythological',
+        author: 'Amish Tripathi',
+        read: false
+    },
+    {
+        title: 'Norse Mythology',
+        genre: 'Fiction',
+        author: 'Neil Gaiman',
+        read: false
+    },
+    {
+        title: 'We Are Okay',
+        genre: 'Fiction',
+        author: 'Nina LaCour',
+        read: false
+    }, {
+        title: 'Pachinko',
+        genre: 'Fiction',
+        author: 'Min Jin Leey',
+        read: false
+    }
+
+
+
+];
+
+var router = function (nav) {
 
     adminRouter.route('/addBooks')
-        .get(function(req,res){
-            res.send('Inserting Books');
-    });
+        .get(function (req, res) {
+            var url = 'mongodb://localhost:27017/libraryApp';
+            mongodb.connect(url, function (err, db) {
+                console.log("Connected correctly to server");
+                var collection = db.collection('books');
+                collection.insertMany(books, function (err, results) {
+                     console.log("Got results");
+                    res.send(results);
+                    db.close();
+                });
+            });
+
+            // res.send('Inserting Books');
+        });
 
     return adminRouter;
 };
 
 module.exports = router;
-    
